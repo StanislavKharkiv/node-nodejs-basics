943e0dcb46a96595c59e0c4a4a2e4a29657eb69c
StanislavKharkiv <s.iosifov@quantumobile.com>
2022-12-02 17:00:44 +0200

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import process from "node:process";
+import { createWriteStream } from "node:fs";
+import { getDirName } from "../helpers/files.mjs";
+import { pipeline, Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const rs = process.stdin;
+  const ws = createWriteStream(
+    `${getDirName(import.meta.url)}/files/fileToWrite.txt`
+  );
+
+  const transform = new Transform({
+    transform(chunk, enc, cb) {
+      const reversed = chunk.toString().split('').reverse().join('');
+      this.push(reversed);
+      cb();
+    },
+  });
+
+  pipeline(rs, transform, ws, err => console.log(err));
 };
 
 await transform();
