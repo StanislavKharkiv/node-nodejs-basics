4949c44127675f2afb7c5f8703365b9b173ed944
StanislavKharkiv <s.iosifov@quantumobile.com>
2022-12-03 21:46:31 +0200

    feat: zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "node:zlib";
+import { createWriteStream, createReadStream } from "node:fs";
+import { getDirName } from "../helpers/files.mjs";
+import { pipeline } from "node:stream";
+
 const compress = async () => {
-    // Write your code here 
+  const filesPath = `${getDirName(import.meta.url)}/files`;
+
+  pipeline(
+    createReadStream(`${filesPath}/fileToCompress.txt`),
+    zlib.createGzip(),
+    createWriteStream(`${filesPath}/archive.gz`),
+    (err) => console.log(err)
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from "node:zlib";
+import { createWriteStream, createReadStream } from "node:fs";
+import { getDirName } from "../helpers/files.mjs";
+import { pipeline } from "node:stream";
+
 const decompress = async () => {
-    // Write your code here 
+  const filesPath = `${getDirName(import.meta.url)}/files`;
+  const rs = await createReadStream(`${filesPath}/archive.gz`);
+  const ws = createWriteStream(`${filesPath}/fileToCompress.txt`);
+  rs.pipe(zlib.createGunzip()).pipe(ws);
 };
 
 await decompress();
