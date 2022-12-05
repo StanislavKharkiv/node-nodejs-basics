86701a3ae14a53b6cec6c9840bd18e3594b8c20f
StanislavKharkiv <s.iosifov@quantumobile.com>
2022-12-05 14:44:47 +0200

    feat: worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,13 @@
+import threads from "node:worker_threads";
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  threads.parentPort.on('message', data => {
+    threads.parentPort.postMessage(nthFibonacci(data));
+    process.exit(0);
+  })
 };
 
 sendResult();
